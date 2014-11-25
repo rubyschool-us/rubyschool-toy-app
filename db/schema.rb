89c35da9089e7cf97b05474fdbeb207acb63ff71
Roman Pushkin <roman.pushkin@gmail.com>
2014-11-25 23:45:44 +0300

    Update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125191809) do
+ActiveRecord::Schema.define(version: 20141125193655) do
+
+  create_table "microposts", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
